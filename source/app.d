41999676d2fb17a55df4cab5df7a2157ade26f38
CodeMyst <code@myst.rs>
2021-11-03 22:42:06 +0100

    changed port

--- source/app.d
+++ source/app.d
@@ -114,7 +114,7 @@ void main()
 
     auto serverSettings = new HTTPServerSettings();
     serverSettings.bindAddresses = ["127.0.0.1"];
-    serverSettings.port = 5000;
+    serverSettings.port = 5005;
     serverSettings.errorPageHandler = toDelegate(&displayError);
 
     router.registerWebInterface(new RootWeb());
