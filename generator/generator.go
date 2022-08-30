f734075ee23e1f3401f77db906f16b26305fdd33
CodeMyst <code@myst.rs>
2022-08-30 15:06:49 +0200

    added config

--- generator/generator.go
+++ generator/generator.go
@@ -1,19 +1,44 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/labstack/echo/v4"
+	"github.com/pelletier/go-toml/v2"
 )
 
+type Config struct {
+	Port  uint64
+	Title string
+}
+
 func main() {
+	if _, err := os.Stat("config.toml"); errors.Is(err, os.ErrNotExist) {
+		fmt.Println("config.toml doesn't exist.")
+		return
+	}
+
+	cfgFile, err := os.ReadFile("config.toml")
+	if err != nil {
+		fmt.Println("Failed to read config.toml: ", err.Error())
+		return
+	}
+
+	var cfg Config
+	err = toml.Unmarshal(cfgFile, &cfg)
+	if err != nil {
+		fmt.Println("Failed to parse config.toml: ", err.Error())
+		return
+	}
+
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "hello from my blog")
 	})
 
-	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", os.Getenv("PORT"))))
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", cfg.Port)))
 }
--- generator/go.mod
+++ generator/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.5
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
