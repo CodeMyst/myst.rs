de50df22c6689ea5e6530f60e070881f90de61e0
CodeMyst <code@myst.rs>
2023-09-19 19:05:30 +0200

    redesigned website

--- pages/about.md
+++ pages/about.md
@@ -1,9 +1,1 @@
-Hello, I am CodeMyst. A programmer who plays around with everything, mainly focusing on web and games. Most of the things I do are open source.
-
-I like working with less known and obscure technologies.
-
-Languages: C#, Java, D, Zig, JavaScript/TypeScript, Python, Rust
-
-Backends: ASP.Net Core, vibe.d
-
-Engines: Unity, MonoGame
+Hello! I am CodeMyst, a programmer who plays around with everything. I mainly focus on web and games. Big supporter of open source and enjoy working with anything that's less known.
--- source/app.d
+++ source/app.d
@@ -4,83 +4,6 @@ import vibe.d;
 import std.datetime;
 import dyaml;
 
-///
-public enum ProjectType
-{
-    web,
-    games,
-    tools,
-}
-
-///
-public struct Project
-{
-    ///
-    string name;
-    ///
-    string link;
-    ///
-    string lang;
-    ///
-    string langBg;
-    ///
-    string langFg;
-    ///
-    string status;
-    ///
-    string time;
-    ///
-    string website;
-    ///
-    string source;
-    ///
-    string description;
-    ///
-    string jam;
-    ///
-    ProjectType projectType;
-    ///
-    uint stars;
-
-    ///
-    this(const Node node)
-    {
-        import std.conv : to;
-
-        name = node["name"].as!string;
-        link = node["link"].as!string;
-        lang = node["lang"].as!string;
-        langBg = node["lang-bg"].as!string;
-        langFg = node["lang-fg"].as!string;
-        time = node["time"].as!string;
-        website = node["website"].as!string;
-        source = node["source"].as!string;
-        description = node["description"].as!string;
-        stars = node["stars"].as!uint;
-
-        if ("jam" in node)
-        {
-            jam = node["jam"].as!string;
-        }
-        else
-        {
-            jam = null;
-        }
-
-        if ("status" in node)
-        {
-            status = node["status"].as!string;
-        }
-        else
-        {
-            status = null;
-        }
-
-        const typeString = node["type"].as!string;
-        projectType = typeString.to!ProjectType;
-    }
-}
-
 /++
  + post struct
  +/
@@ -96,15 +19,8 @@ public struct Post
     string path;
 }
 
-///
-string pat;
-
 void main()
 {
-    pat = getPat();
-
-    // cacheStars();
-
     auto router = new URLRouter();
 
     auto fsettings = new HTTPFileServerSettings();
@@ -118,8 +34,6 @@ void main()
     serverSettings.errorPageHandler = toDelegate(&displayError);
 
     router.registerWebInterface(new RootWeb());
-
-    setTimer(1.hours, toDelegate(&cacheStars), true);
 
     listenHTTP(serverSettings, router);
 
@@ -148,18 +62,7 @@ class RootWeb
     @path("/")
     void getIndex()
     {
-        Node root = Loader.fromFile("projects.yaml").load();
-
-        Project[] projects;
-
-        foreach (project; root["projects"].sequence)
-        {
-            auto p = project.as!Project();
-
-            projects ~= p;
-        }
-
-        render!("index.dt", projects);
+        render!("index.dt");
     }
 
     @path("/blog")
@@ -239,100 +142,3 @@ Post[] getPosts()
     return posts;
 }
 
-uint getRemainingRateLimit()
-{
-    uint rem = 0;
-
-    const url = "https://api.github.com/rate_limit";
-
-    requestHTTP(url,
-        (scope req)
-        {
-            req.headers.addField("Authorization", "token " ~ pat);
-            req.method = HTTPMethod.GET;
-        },
-
-        (scope res)
-        {
-            if (res.statusCode == HTTPStatus.ok)
-            {
-                auto json = parseJsonString(res.bodyReader.readAllUTF8());
-
-                rem = json["resources"]["core"]["remaining"].get!uint;
-            }
-        }
-    );
-
-    return rem;
-}
-
-uint getStars(Project p)
-{
-    import std.algorithm : startsWith;
-
-    uint stars = 0;
-
-    if (!p.source.startsWith("https://github.com/"))
-    {
-        return stars;
-    }
-
-    if (getRemainingRateLimit() == 0)
-    {
-        return stars;
-    }
-
-    const repo = p.source["https://github.com/".length..$];
-
-    const url = "https://api.github.com/repos/" ~ repo;
-
-    requestHTTP(url,
-        (scope req)
-        {
-            req.headers.addField("Authorization", "token " ~ pat);
-            req.method = HTTPMethod.GET;
-        },
-
-        (scope res)
-        {
-            if (res.statusCode == HTTPStatus.ok)
-            {
-                auto json = parseJsonString(res.bodyReader.readAllUTF8());
-
-                stars = json["stargazers_count"].get!uint;
-            }
-        }
-    );
-
-    return stars;
-}
-
-///
-string getPat()
-{
-    import std.file : readText;
-    import std.string : strip;
-
-    return readText("pat.txt").strip();
-}
-
-///
-void cacheStars()
-{
-    import std.stdio : File;
-    import std.conv : to;
-
-    Node root = Loader.fromFile("projects.yaml").load();
-
-    foreach (project; root["projects"].sequence)
-    {
-        auto p = project.as!Project;
-        project["stars"] = getStars(p);
-    }
-
-    auto f = File("projects.yaml", "w");
-
-    dumper().dump(f.lockingTextWriter, root);
-
-    f.close();
-}
